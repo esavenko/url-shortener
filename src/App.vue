d60072c5e272718bdbea61df0d8d331fee7f1926
Egor Savenko <smahelzed@gmail.com>
2025-01-21 09:38:36 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid aspernatur,
+  at atque blanditiis dicta doloremque doloribus dolorum earum esse ipsam itaque
+  libero magni minus molestias perspiciatis quos ratione rem, veniam?
+</template>
